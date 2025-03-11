674996bd7487cdc074a56fabd08c8db110facae3
joaovitoralbernaz <159267082+joaovitoralbernaz@users.noreply.github.com>
2025-03-11 11:17:43 -0300

    Update main.c

--- main.c
+++ main.c
@@ -45,3 +45,75 @@ int main() {
 }
 
 ---------------------------------------------------------------------------------
+
+Faça um programa que conte de 0 a 20, mas só imprima os números pares.
+
+#include <stdio.h>
+
+int main() {
+    int n = 0;
+    
+    while(n <= 20 && n >= 0){
+        if(n % 2 == 0){
+            printf(" %d \n" , n);
+        }
+        n = n + 1;
+    }
+
+    return 0;
+}
+
+-------------------------------------------------------------------------------------
+ Escreva um programa que peça ao usuário para adivinhar um número e informe se o palpite está correto.
+
+#include <stdio.h>
+
+int main(){
+    int n = 10;
+    int resp;
+    printf("Advinhe o número de 0 a 50: ");
+    scanf("%d" , &resp);
+    
+    if(resp != n){
+        printf("Você errou o número :(");
+    }
+    else{
+        printf("Você acertou o número!");
+    }
+    
+    return 0;
+}
+
+----------------------------------------------------------------------------------------
+ Crie um programa que imprima os números de 10 a 1 em ordem decrescente.
+
+#include <stdio.h>
+
+int main(){
+    int n = 10;
+    
+    while(n > 0){
+        printf("%d \n" , n);
+        n = n-1;
+    }
+    
+    return 0;
+}
+
+----------------------------------------------------------------------------------------------------
+      • Faça um programa que calcule a soma dos números de 1 a 50 usando um loop while.
+
+#include <stdio.h>
+
+int main(){
+    int n = 1;
+    int soma = 0;
+    
+    while( n < 50){
+        soma = soma + n;
+        n++;
+        
+    }
+    printf("A soma é: %d" , soma);
+    return 0;
+}
