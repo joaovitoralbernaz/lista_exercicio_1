802d151125eba9adb05af2d1b38da4fc05f316c4
joaovitoralbernaz <159267082+joaovitoralbernaz@users.noreply.github.com>
2025-03-22 20:45:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,51 +1,41 @@
-#include <stdio.h>
-
-int main(){
-    int a = 0;
-    while ( a < 10){
-        a++;
-        printf("%d \n" , a);
-        
-    }
-    return 0 ;
-}
-
------------------------------------------------------------------------------------------------------------
-
-#include <stdio.h>
-
-int main(){
-    int n1;
-    printf("Digite um número inteiro: ");
-    scanf("%d" , &n1);
-        
-    if( n1 < 0){
-        printf("O número é negativo");
-    }else{
-        printf("O número é positivo");
-    }
-    return 0 ;
-}
-
---------------------------------------------------------------------------------------------------------------
+Escreva um programa que imprima os números de 1 a 10 usando um loop while.
 
 #include <stdio.h>
 
 int main() {
-    int n = 0;
+    int i = 1;
     
-    while(n <= 20 && n >= 0){
-        if(n % 2 == 0){
-            printf(" %d \n" , n);
-        }
-        n = n + 1;
+    while(i<=10){
+        printf("%d \n" , i);
+        i++;
     }
 
     return 0;
 }
 
-----------------------------------------------------------------------------------------------------------------
--
+------------------------------------------------------------------------------------------------------------------------------------------------
+ Crie um programa que solicite um número ao usuário e imprima se ele é positivo, negativo ou zero.
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Digite um número: ");
+    scanf("%d" , &a);
+    
+    if(a < 0){
+        printf("O número é negativo");
+    } else if(a > 0){
+        printf("O número é positivo");
+    }
+    else{
+        printf("O número é 0");
+    }
+
+    return 0;
+}
+
+------------------------------------------------------------------------------------------------------------------------------------------------
 Faça um programa que conte de 0 a 20, mas só imprima os números pares.
 
 #include <stdio.h>
@@ -63,7 +53,7 @@ int main() {
     return 0;
 }
 
----------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------
  Escreva um programa que peça ao usuário para adivinhar um número e informe se o palpite está correto.
 
 #include <stdio.h>
@@ -84,7 +74,7 @@ int main(){
     return 0;
 }
 
--------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------
  Crie um programa que imprima os números de 10 a 1 em ordem decrescente.
 
 #include <stdio.h>
@@ -100,110 +90,129 @@ int main(){
     return 0;
 }
 
--------------------------------------------------------------------------------------------------------------------
- • Faça um programa que calcule a soma dos números de 1 a 50 usando um loop while.
+------------------------------------------------------------------------------------------------------------------------------------------------
+Faça um programa que calcule a soma dos números de 1 a 50 usando um loop while.
 
 #include <stdio.h>
 
-int main(){
-    int n = 1;
+int main() {
     int soma = 0;
+    int n = 1;
     
-    while( n <= 50){
+    while(n <= 50){
         soma = soma + n;
         n++;
-        
+        printf("a soma vale %d \n" , soma);
     }
-    printf("A soma é: %d" , soma);
+
     return 0;
 }
 
-----------------------------------------------------------------------------------------------------------------------
-• Escreva um programa que solicite a idade do usuário e informe se ele é uma criança, adolescente, adulto ou idoso.
+------------------------------------------------------------------------------------------------------------------------------------------------
+Escreva um programa que solicite a idade do usuário e informe se ele é uma criança, adolescente, adulto ou idoso.
 
 #include <stdio.h>
 
-int main(){ 
-    int idd;
-    
+int main() {
+    int idade;
     printf("Digite sua idade: ");
-    scanf("%d" , &idd);
+    scanf("%d" , &idade);
     
-    if( idd > 65){
-        printf("Você é idoso");
-        
-    }else if ( idd > 20){
-        printf("Você é adulto");
-        
-    }else if ( idd > 13){
-        printf("Você é adolescente");
+    if(idade < 12){
+        printf("Você é uma criança!");
+    }else if(idade < 18){
+        printf("Você é um adolescente!");
+    }else if(idade < 60){
+        printf("Você é um adulto!");
     }else{
-        printf("Você é criança");
+        printf("Você é um idoso!");
     }
+
     return 0;
 }
 
-----------------------------------------------------------------------------------------------------------------------------
-  • Crie um programa que conte de 1 a 100, mas só imprima os números divisíveis por 7.
+------------------------------------------------------------------------------------------------------------------------------------------------
+Crie um programa que conte de 1 a 100, mas só imprima os números divisíveis por 7.
 
 #include <stdio.h>
 
-int main(){ 
-    int n = 1;
-    printf("Os numeros divisíveis por 7 de 1 a 100 são:\n ");
-    while( n <= 100){
-        if( n % 7 == 0){
-            printf("%d\n" , n );
+int main() {
+    int n = 0;
+    
+    while(n <= 100 && n >= 0){
+        if(n % 7 == 0){
+            printf(" %d \n" , n);
         }
-        n++;
+        n = n + 1;
     }
+
     return 0;
 }
 
-------------------------------------------------------------------------------------------------------------------------------
-• Faça um programa que peça dois números ao usuário e imprima o maior deles.
+------------------------------------------------------------------------------------------------------------------------------------------------
+Faça um programa que peça dois números ao usuário e imprima o maior deles.
 
 #include <stdio.h>
 
-int main(){ 
+int main() {
     int a;
     int b;
-    
-    printf("Digite o primeiro numero: ");
-    scanf("%d" , &a);
+    printf("Digite o primeiro número: ");
+    scanf(" %d", &a);
     printf("Digite o segundo número: ");
-    scanf("%d" , &b);
+    scanf(" %d" , &b);
     
-    if( a > b){
-        printf("O MAIOR NÚMERO É: %d" , a);
+    if(a < b){
+        printf(" %d é o maior número", b);
     }else{
-        printf("O MAIOR NÚMERO É: %d" , b);
+        printf(" %d é o maior número", a);
     }
+
     return 0;
 }
 
-------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------
+Crie um programa que peça um número ao usuário e imprima se é ímpar ou par.
 
- • Crie um programa que peça um número ao usuário e imprima se é ímpar ou par.
+#include <stdio.h>
 
-    #include <stdio.h>
-
-int main(){
+int main() {
     int n;
-    printf("Digite um número: ");
+    printf("Escreva um número: ");
     scanf("%d" , &n);
     
-    if( n % 2 == 0 ){
-        printf("O número é par");
+    if(n % 2 == 0){
+        printf("Esse número é par!");
     }else{
-        printf("O número e ímpar");
+        printf("Esse número é ímpar!");
     }
+
     return 0;
 }
 
-----------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------
+Faça um programa que conte de 1 a 30, mas pule os números múltiplos de 3.
 
- • Faça um programa que conte de 1 a 30, mas pule os números múltiplos de 3.
+
+#include <stdio.h>
+
+int main() {
+    int n = 0;
+    
+    while(n < 30){
+        if(n % 3 == 0){
+            printf("\nEsse número é múltiplo de 3");
+            n++;
+        }else{
+            printf("\n %d", n);
+            n++;
+        }
+    }
+    
+    return 0;
+}
+
+OU
 
 #include <stdio.h>
 
@@ -222,54 +231,49 @@ int main() {
     return 0;
 }
 
-------------------------------------------------------------------------------------------------------------------------------------------
-
- • Escreva um programa que solicite dois números e informe se são iguais.
+------------------------------------------------------------------------------------------------------------------------------------------------
+ Escreva um programa que solicite dois números e informe se são iguais.
 
 #include <stdio.h>
 
-int main (){
-    int n1;
-    int n2;
+int main() {
+    int a;
+    int b;
+    printf("Digite um número número: ");
+    scanf(" %d" , &a);
+    printf("Digite um outro número número: ");
+    scanf(" %d" , &b);
     
-    printf("Digite um número: ");
-    scanf("%d" , &n1 );
-    printf("Digite um segundo número: ");
-    scanf("%d" , &n2);
-    
-    if( n1 == n2 ){
-        printf("Os números são iguais!!");
+    if(a == b){
+        printf("Os números são iguais");
     }else{
         printf("Os números são diferentes");
     }
+
     return 0;
 }
 
 ------------------------------------------------------------------------------------------------------------------------------------------------
-
- • Crie um programa que imprima a tabuada de multiplicação de um número fornecido pelo usuário.
+Crie um programa que imprima a tabuada de multiplicação de um número fornecido pelo usuário.
 
 #include <stdio.h>
 
-int main (){
-    int a = 1;
-    int b;
-    
+int main() {
+    int a;
+    int c= 1;
     printf("Digite um número: ");
-    scanf("%d" , &b);
+    scanf("%d" , &a);
     
-    while( a <= 10){
-        printf("%d \n" , a * b);
-        a++;
-        
+    while(c <= 10){
+        printf("%d\n" , c * a);
+        c++;
     }
 
     return 0;
 }
 
---------------------------------------------------------------------------------------------------------------------------------------------------------
-
- • Faça um programa que conte de 1 a 100 e imprima "Fizz" para números divisíveis por 3 e "Buzz" para números divisíveis por 5. Para números que são divisíveis por ambos, imprima "FizzBuzz".
+------------------------------------------------------------------------------------------------------------------------------------------------
+Faça um programa que conte de 1 a 100 e imprima "Fizz" para números divisíveis por 3 e "Buzz" para números divisíveis por 5. Para números que são divisíveis por ambos, imprima "FizzBuzz".
 
 #include <stdio.h>
 
@@ -292,4 +296,110 @@ int main() {
     return 0;
 }
 
+------------------------------------------------------------------------------------------------------------------------------------------------
+Escreva um programa que solicite um número e verifique se está na sequência de Fibonacci. ( 0+1=1; 1+1=2; 1+2=3; 2+3=5; 3+5=8; 5+8=13).0,1,1,2,3,5,8,13. 
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+ Crie um programa que peça a altura e o peso do usuário e calcule o índice de massa corporal (IMC).
+
+#include <stdio.h>
+
+int main(){
+    float alt;
+    float peso;
+
+    printf("Digite sua altura: ");
+    scanf(" %f", &alt);
+    printf("Digite seu peso: ");
+    scanf(" %f", &peso);
+   
+    int imc = peso/(alt*alt);
+    printf("Seu IMC é: %d ", imc);
     
+    
+    return 0;
+}
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+Faça um programa que solicite um número e imprima se é primo ou não.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    int a;
+    int primo = 1;
+    printf("Digite um número: ");
+    scanf("%d", &n);
+
+    if(n < 2){
+        primo = 0;
+    }else{
+        for(a = 2; a * a <= n; a++){
+            if(n % a == 0){
+                primo = 0;
+                break;
+            }
+        }
+    }
+    if (primo){
+        printf("É um número primo!\n");
+    }
+    else{
+        printf("Não é um número primo!\n");
+    }
+    return 0;
+}
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+Escreva um programa que simule um jogo de adivinhação, onde o computador escolhe um número aleatório e o usuário tenta adivinhar.
+
+#include <stdio.h>
+
+int main() {
+    int n = 10;
+    int a;
+    printf("Tente advinhar o número: ");
+    scanf("%d" , &a);
+    
+    if(a == n){
+        printf("Você acertou o número!");
+    }else{
+        printf("Você errou o número :(");
+    }
+    
+    
+
+    return 0;
+}
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+Crie um programa que imprima os números primos de 1 a 50.
+
+Números primos:2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    int a; 
+    int primo;
+    printf("Números primos de 1 a 50:\n");
+
+    for (num = 1; num <= 50; num++) {
+        if (num < 2) continue;
+        primo = 1;
+        for (a = 2; a * a <= num; a++) {
+            if (num % a == 0) {
+                primo = 0;
+                break;
+            }
+        }if (primo) {
+            printf("%d \n", num);
+        }
+    }
+    return 0;
+} 
